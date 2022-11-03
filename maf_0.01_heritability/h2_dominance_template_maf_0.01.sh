31aad4135444bfd73360a48375735ab71831b12c
astheeggeggs <duncan.stuart.palmer@gmail.com>
2022-11-03 15:36:29 +0000

    renamed the output file

--- maf_0.01_heritability/h2_dominance_template_maf_0.01.sh
+++ maf_0.01_heritability/h2_dominance_template_maf_0.01.sh
@@ -20,6 +20,6 @@ source activate d-ldsc
 
 ldscore_files="/stanley/genetics_storage/analysis/ukbb_dominance/ldscores_ldscgxe/ldscores_maf_0.01_nov_2022/1000G.EUR.QC.chr@";
 ldscore_files_weights="/stanley/genetics_storage/analysis/ukbb_dominance/ldscores_ldscgxe/ldscores_maf_0.01_nov_2022/1000G.EUR.QC.chr@";
-out="${path_to_out}/dom.alpha.${alpha}.${phenotype}" 
+out="${path_to_out}/dom.${phenotype}" 
 echo "./get_h2.py --dominance --ref-ld-chr $ldscore_files --w-ld-chr $ldscore_files_weights --n-blocks 200 --write-h2 --h2 $sumstat_file --chisq-max 10000000000 --out $out --pheno-name $phenotype"
 python /home/unix/dpalmer/Repositories/d-ldsc/get_h2.py --dominance --ref-ld-chr $ldscore_files --w-ld-chr $ldscore_files_weights --n-blocks 200 --write-h2 --h2 $sumstat_file --chisq-max 10000000000 --out $out --pheno-name $phenotype
