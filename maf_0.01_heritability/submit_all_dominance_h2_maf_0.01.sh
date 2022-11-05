2e053cba6c85ab965c11b015d87bc51f4a7ac3d9
astheeggeggs <duncan.stuart.palmer@gmail.com>
2022-11-05 12:52:25 +0000

    updated submission of new ldsc estimation

--- maf_0.01_heritability/submit_all_dominance_h2_maf_0.01.sh
+++ maf_0.01_heritability/submit_all_dominance_h2_maf_0.01.sh
@@ -4,13 +4,15 @@
 # scp ~/Repositories/ldscgxe/1000G_EUR_Phase3_plink/ldscores_maf_0.01/* dpalmer@login01:/stanley/genetics_storage/analysis/ukbb_dominance/ldscores_ldscgxe/ldscores_maf_0.01_nov_2022/
 
 # Run it
-sumstats_folder=$1; 		# /stanley/genetics_storage/analysis/ukbb_dominance/ldsc-dominance-export/sumstats-files-1, 2, etc;
+sumstats_folder=$1; 		# /psych/genetics_data/dpalmer/UKbb/ldsc-dominance-export
 path_to_output=$2; 			# /psych/genetics_data/dpalmer/h2-maf-0.01/;
+grep=$3;
 
+path_to_output="/psych/genetics_data/dpalmer/UKbb/h2_maf_0.01_results"
 mkdir $path_to_output;
 
-for sumstat_filename in $sumstats_folder/*both_sexes*; do
+for sumstat_filename in $sumstats_folder/${grep}*both_sexes*; do
 	phenotype=`echo $sumstat_filename | sed -e "s/^[\.\.//]*//" | sed -e "s/\..*//" | sed -e "s/^.*\///"`;
 	echo "$phenotype submitted to the cluster for dominance heritability estimation";
-	qsub -v sumstat_file="$sumstat_filename",phenotype="$phenotype",path_to_out="${path_to_output}" h2_dominance_template_maf_0.01.sh
+	# qsub -v sumstat_file="$sumstat_filename",phenotype="$phenotype",path_to_out="${path_to_output}" h2_dominance_template_maf_0.01.sh
 done
